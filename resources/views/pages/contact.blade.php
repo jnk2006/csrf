ef45d269eeeb364aeb2e02e76458012ef70721e7
jnk2006 <jnk22@njit.edu>
2018-10-17 09:51:58 -0400

    Feature: using csrf, print the email and message that was submitted in the form.

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,35 +6,18 @@
     <p class="lead">Please use this form to contact.</p>
 
 
-    <form>
+    <form action="/contact" method="post">
+        {{ csrf_field() }}
         <div class="form-group">
-            <label for="exampleFormControlInput1">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            <label for="email">Email address</label>
+            <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
         </div>
+
         <div class="form-group">
-            <label for="exampleFormControlSelect1">Example select</label>
-            <select class="form-control" id="exampleFormControlSelect1">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </select>
+            <label for="body">Message</label>
+            <textarea name="body" class="form-control" id="body" rows="3"></textarea>
         </div>
-        <div class="form-group">
-            <label for="exampleFormControlSelect2">Example multiple select</label>
-            <select multiple class="form-control" id="exampleFormControlSelect2">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlTextarea1">Example textarea</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
+        <button type="submit" class="btn btn-primary mb-2">Submit</button>
     </form>
 
 @endsection
