c6acd14ae55bf6092bfa04e052823061348ced65
jnk2006 <jnk22@njit.edu>
2018-10-18 19:38:05 -0400

    Feature: print thank you message with the name after submit is clicked

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="card mt-3 pl-2 pr-2">
-        <div class="card-title"><h1>Contact</h1>
+        <div class="card-title"><h1>Contact Us</h1>
             <p class="lead">Please use this form to contact.</p></div>
         <div class="card-body">
             <form action="{{route('contact.store')}}" method="post">
