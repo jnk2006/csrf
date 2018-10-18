95bc4e914dd1938d729ec06a82ab4d30bf15a652
jnk2006 <jnk22@njit.edu>
2018-10-18 19:01:50 -0400

    Fix: edited the homepage of the website added style in jumbtron.css

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,38 +3,49 @@
 @section('content')
 
 
-        <!-- Main jumbotron for a primary marketing message or call to action -->
-        <div class="jumbotron">
-            <div class="container">
-                <h1 class="display-3">Hello, world!</h1>
-                <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-                <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron jumbotron-fluid bg-dark">
+        <div class="jumbotron-background">
+            <img src="https://us.123rf.com/450wm/mitoimages/mitoimages1705/mitoimages170510083/78802448-close-up-of-raw-food-on-kitchen-counter-for-chicken-soup-munich-bavaria-germany.jpg" class="blur"
+            width="1100px" height="500px">
+        </div>
+        <div class="container">
+            <h1 class="display-3">Welcome to The Bouffage</h1>
+            <h4> The only site that enables you to obtain the recipes based on the ingredients available in your
+                    house.</h4>
+            <p><a class="btn btn-primary btn-md" href="#" role="button">Click here</a> To enter your ingredients</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-md-4">
+                <figure>
+                    <h3>Tips</h3>
+                    <img src="http://www.listotic.com/wp-content/uploads/2014/04/The-BEST-list-of-kitchen-tips-and-tricks-I-didnt-know-about-most-of-these..jpg"
+                         width="300px" height="350px">
+                </figure>
+            </div>
+            <div class="col-md-4">
+                <figure>
+                    <h3>Bookmarks</h3>
+                    <img src="http://www.thefoodsection.com/.a/6a00d8341c4ec753ef0120a5e4a05c970c-pi" width="300px"
+                         height="400px">
+                </figure>
+            </div>
+            <div class="col-md-4">
+                <figure>
+                    <h3>Shopping Lists</h3>
+                    <img src="http://templatelab.com/wp-content/uploads/2018/03/grocery-list-template-10.jpg"
+                         width="400px" height="400px">
+                </figure>
             </div>
         </div>
 
-        <div class="container">
-            <!-- Example row of columns -->
-            <div class="row">
-                <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-                </div>
-                <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-                </div>
-                <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-                </div>
-            </div>
+        <hr>
 
-            <hr>
-
-        </div> <!-- /container -->
+    </div> <!-- /container -->
 
 
 @endsection
